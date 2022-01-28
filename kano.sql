d2a7150fa37ac1afc9e5a8955691d2c432727062
pauuii <98545643+pauuii@users.noreply.github.com>
2022-01-28 08:26:11 +0800

    Update kano.sql

--- kano.sql
+++ kano.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `kano_rank`;
-CREATE TABLE `kano_rank` (
+DROP TABLE IF EXISTS `rin_rank`;
+CREATE TABLE `rin_rank` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `score` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
